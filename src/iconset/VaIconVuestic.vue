d14b1cca64f313b56f90ca1161c22993d38ccdf0
championshuttler <shivams2799@gmail.com>
2020-07-07 19:50:13 -0400

    Fix the icon height and width in header

--- src/iconset/VaIconVuestic.vue
+++ src/iconset/VaIconVuestic.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="../assets/icons/logo.png" :style="logoStyle"/>
+  <img src="../assets/icons/logo.png" :style="logoStyle" :adaptive="true" />
 </template>
 <script>
 
@@ -10,6 +10,8 @@ export default {
     logoStyle () {
       return {
         color: 'white',
+        width: 'auto',
+        height: 'auto',
       }
     },
     themeGradientId () {
