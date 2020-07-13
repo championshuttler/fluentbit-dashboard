ed1b49ea802b7b0b7a840ad0959082131d62e6a2
championshuttler <shivams2799@gmail.com>
2020-07-13 08:56:38 -0400

    Add the different colors for the different charts

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,11 @@
 const config = state => state.app.config
 const palette = state => state.app.config.palette
 const isLoading = state => state.app.isLoading
+const graphColor = state => state.app.color
 
 export {
   config,
   palette,
   isLoading,
+  graphColor,
 }
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,6 +2,56 @@ const state = {
   sidebar: {
     opened: false,
   },
+  graphColors: [
+    {
+      color: 'red',
+      taken: false,
+    },
+    {
+      color: 'rgba(17, 99, 156,0.4)',
+      taken: false,
+    },
+    {
+      color: 'blue',
+      taken: false,
+    },
+    {
+      color: 'orange',
+      taken: false,
+    },
+    {
+      color: 'grey',
+      taken: false,
+    },
+    {
+      color: 'pink',
+      taken: false,
+    },
+    {
+      color: 'violet',
+      taken: false,
+    },
+    {
+      color: 'rgba(17, 99, 156,0.4)',
+      taken: false,
+    },
+    {
+      color: 'rgba(17, 99, 156,0.4)',
+      taken: false,
+    },
+    {
+      color: 'blue',
+      taken: false,
+    },
+    {
+      color: 'blue',
+      taken: false,
+    },
+    {
+      color: 'blue',
+      taken: false,
+    },
+  ],
   config: {
     palette: {
       primary: '#4ae387',
@@ -23,9 +73,22 @@ const mutations = {
   setLoading (state, isLoading) {
     state.isLoading = isLoading
   },
+  changeColor (state, index) {
+    state.graphColors[index].taken = true
+  },
 }
 
 const actions = {
+  updateColor ({ state, commit }) {
+    const colors = state.graphColors
+    for (let index = 0; index < colors.length; index++) {
+      const color = colors[index]
+      if (!color.taken) {
+        commit('changeColor', index)
+        return color.color
+      }
+    }
+  },
 }
 
 export default {
