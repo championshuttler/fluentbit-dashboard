1bee539bafbcba155f46c71c7196ee650fcf8da8
championshuttler <shivams2799@gmail.com>
2020-07-13 09:15:09 -0400

    Add the counter clock on the charts
    Signed-off-by: championshuttler <shivams2799@gmail.com>

--- src/components/plugins/Chart.vue
+++ src/components/plugins/Chart.vue
@@ -2,7 +2,19 @@
   <div class="flex md12 xs12 lg12">
     <va-card :style="computedStyle">
       <template slot="header">
-        <h5 class="mt-0 mb-0">{{pluginName.toUpperCase()}}</h5>
+        <div class="row">
+          <div class="flex lg11">
+            <h5 class="mt-0 mb-0">{{pluginName.toUpperCase()}}</h5>
+          </div>
+          <div class="flex lg1">
+            <div id="countdown">
+              <div id="countdown-number">{{counter}}</div>
+              <svg>
+                <circle r="18" cx="20" cy="20"></circle>
+              </svg>
+            </div>
+          </div>
+        </div>
       </template>
       <div class="row">
         <template v-for="(metric, index) in metrics">
@@ -18,6 +30,12 @@ import LineChart from '../chart/LineChart'
 import _ from 'lodash'
 
 export default {
+  mounted () {
+    setInterval(() => {
+      if (this.counter === 0) this.counter = 10
+      this.counter--
+    }, 1000)
+  },
   computed: {
     computedStyle () {
       return {
@@ -81,6 +99,7 @@ export default {
   },
   data () {
     return {
+      counter: 10,
       metrics: [],
       dataset: {},
       previousData: {},
@@ -88,3 +107,50 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+#countdown {
+  display: inline-block;
+  position: relative;
+  margin: auto;
+  margin-top: 5px;
+  height: 40px;
+  width: 40px;
+  text-align: center;
+}
+
+#countdown-number {
+  color: white;
+  display: inline-block;
+  line-height: 40px;
+}
+
+svg {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 40px;
+  height: 40px;
+  transform: rotateY(-180deg) rotateZ(-90deg);
+}
+
+svg circle {
+  stroke-dasharray: 113px;
+  stroke-dashoffset: 0;
+  stroke-linecap: round;
+  stroke-width: 2px;
+  stroke: white;
+  fill: none;
+  animation: countdown 10s linear infinite forwards;
+}
+
+@keyframes countdown {
+  from {
+    stroke-dashoffset: 0;
+  }
+
+  to {
+    stroke-dashoffset: 113px;
+  }
+}
+</style>
